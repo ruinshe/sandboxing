fdbf25e9927e9319eb86d71672b5963ae7a77e5c
Ruins He <lyhypacm@gmail.com>
2020-01-03 14:55:13 +0800

    Manually download jdt jar.
    Signed-off-by: Ruins He <lyhypacm@gmail.com>

--- jdt/build.sh
+++ jdt/build.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env zsh
-wget https://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz
-./mvnw -Djdt.js.server.root=$HOME/.emacs.d/eclipse.jdt.ls/server/ -Djunit.runner.root=$HOME/.emacs.d/eclipse.jdt.ls/test-runner/ -Djunit.runner.fileName=junit-platform-console-standalone.jar -Djava.debug.root=$HOME/.emacs.d/eclipse.jdt.ls/server/bundles clean package -Djdt.download.url=file://$PWD/jdt-language-server-latest.tar.gz
+if [ -f jdt-language-server-latest.tar.gz ]; then
+    ./mvnw -Djdt.js.server.root=$HOME/.emacs.d/eclipse.jdt.ls/server/ -Djunit.runner.root=$HOME/.emacs.d/eclipse.jdt.ls/test-runner/ -Djunit.runner.fileName=junit-platform-console-standalone.jar -Djava.debug.root=$HOME/.emacs.d/eclipse.jdt.ls/server/bundles clean package -Djdt.download.url=file://$PWD/jdt-language-server-latest.tar.gz
+else
+    echo please run the command below:
+    echo wget https://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz
+fi
